9ac474c83a49c962b84e8bfba69b1c927a3ca87a
User <user@codeinstitute.net>
2025-01-14 17:44:11 +0100

    Add DRF, Profiles view and serializer

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -16,7 +16,9 @@ import os
 if os.path.exists('env.py'):
     import env
 
-CLOUDINARY_STORAGE = os.environ.get('CLOUDINARY_URL')
+CLOUDINARY_STORAGE =  {
+    'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL')
+}
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
@@ -47,6 +49,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'rest_framework',
     'profiles'
 ]
 
